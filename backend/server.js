556541529e03bb4a7eb560b5a2adadba0a2bb35a
noel.fredrick <noel.fredrick@cheersin.com>
2024-06-16 15:10:45 +0530

    feat: final changes

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const connectDB = require('./config/db');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const itemRoutes = require('./routes/itemRoutes');
 const authRoutes = require('./routes/authRoutes')
 const dotenv = require('dotenv');
@@ -13,12 +12,11 @@ dotenv.config();
 const app = express();
 connectDB();
 
-// app.use(cors());
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
 }));
-// app.use(bodyParser.json());
+
 app.use(express.json());
 
 app.use('/api', itemRoutes);
@@ -32,7 +30,6 @@ app.get('*', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
